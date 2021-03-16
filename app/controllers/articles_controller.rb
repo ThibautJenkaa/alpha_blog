b98080a0c7e05daca8f943bab5fca20da2a4715e
Thibaut <thibaut@jenkaa.com>
2021-03-16 15:10:32 +0100

    add one-to-many asssociations between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "L'article à bien été crée."
       redirect_to @article
